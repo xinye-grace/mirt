5edf3a8fab26ab616b9978e27ec45c80c8ffbc54
xin.ye <xin.ye@cri-united-imaging.com>
2023-08-11 17:10:27 +0800

    Kspace visualize
    Visualize k-space trajectory with same direction as imshow
    Add 'cart:y/2' (image with wrapping)
    Rename variable K -> kspace data, kspace -> traj

--- test.m
+++ test.m
@@ -29,15 +29,27 @@ J = [6 6];
 nufft_args = {N, J, 2*N, N/2, 'table', 2^10, 'minmax:kb'};
 % Am = Gmri(kspace, mask, 'fov', fov, 'nufft', nufft_args);
 
+% Visualize kspace traj (with same direction as imshow/matrix display)
+figure
+plot(omega(:,2), omega(:,1))
+axis([-pi pi -pi pi])
+set(gca,'Ydir','reverse')
+xlabel('Y/Phase Encoding')
+ylabel('X/Frequency Encoding')
+
 %% Simulated kspace data
 
-K = fftshift(ifft2(fftshift(I0)));
+K0 = fftshift(fft2(fftshift(I0)));
+K = K0 + 0.01 * complex(randn(size(K0)), randn(size(K0)));
 switch traj_type
     case 'cartesian'
-        kspace = K(:);
+        K = K(:);
+    case 'cart:y/2'
+        K = K(:,1:2:end);
+        K = K(:);
     case 'half+8'
-        kspace = K(:,1:N(1)/2+1+8);
-        kspace = kspace(:);
+        K = K(:,1:N(1)/2+1+8);
+        K = K(:);
 end
 
 %% Recon
@@ -45,19 +57,19 @@ end
 wi_basis = wi_traj ./ Am.arg.basis.transform;
 
 printm 'conj. phase reconstruction'
-xcp = Am' * (wi_basis .* kspace);
+xcp = Am' * (wi_basis .* K);
 xcp = embed(xcp, mask);
 subplot(1,2,1)
 imshow(abs(xcp),[0,max(max(abs(xcp)))]);
-max(max(abs(xcp)))
+title('conj. phase reconstruction');
 
 printm 'PCG with quadratic regularizer'
 beta = 2^-7 * size(omega,1);
 R = Reg1(mask, 'beta', beta);
 C = R.C;
 niter = 10;
-xpcg = qpwls_pcg(0*xcp(:), Am, 1, kspace(:), 0, C, 1, niter);
+xpcg = qpwls_pcg(0*xcp(:), Am, 1, K(:), 0, C, 1, niter);
 xpcg = embed(xpcg(:,end), mask);
 subplot(1,2,2)
 imshow(abs(xpcg),[0,max(max(abs(xpcg)))]);
-max(max(abs(xpcg)))
+title('PCG with quadratic regularizer');
